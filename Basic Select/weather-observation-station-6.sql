8da70ea85271a458ffd13c79b9dde2b86e7ecd13
Freddy-Liu <ronghanliufreddy@gmail.com>
2021-04-22 16:31:35 -0400

    Update weather-observation-station-6.sql

--- Basic Select/weather-observation-station-6.sql
+++ Basic Select/weather-observation-station-6.sql
@@ -1,7 +1,5 @@
--- Weather Observation Station 6
--- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
 -- https://www.hackerrank.com/challenges/weather-observation-station-6/problem
 
-SELECT DISTINCT City
-FROM Station
-WHERE LEFT(City, 1) IN ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+SELECT DISTINCT CITY
+FROM STATION
+WHERE(CITY LIKE 'a%' OR CITY LIKE 'i%' OR CITY LIKE 'o%' OR CITY LIKE 'e%' OR CITY LIKE 'u%')

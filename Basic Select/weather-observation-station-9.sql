93dc44e32a2a4848c63be91179418070aa808cc5
Freddy-Liu <ronghanliufreddy@gmail.com>
2021-04-22 16:42:07 -0400

    Update weather-observation-station-9.sql

--- Basic Select/weather-observation-station-9.sql
+++ Basic Select/weather-observation-station-9.sql
@@ -1,7 +1,5 @@
--- Weather Observation Station 9
--- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
 -- https://www.hackerrank.com/challenges/weather-observation-station-9/problem
 
-SELECT DISTINCT City
-FROM Station
-WHERE LEFT(City, 1) NOT IN ('A', 'E', 'I', 'O', 'U');
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT(CITY LIKE 'a%' OR CITY LIKE 'i%' OR CITY LIKE 'o%' OR CITY LIKE 'e%' OR CITY LIKE 'u%')

a99d22574ce7648bac5e9fb30251ed5f3e593716
Freddy-Liu <ronghanliufreddy@gmail.com>
2021-04-23 20:53:04 -0400

    Update employee-names.sql

--- Basic Select/employee-names.sql
+++ Basic Select/employee-names.sql
@@ -1,7 +1,5 @@
--- Employee Names
--- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
 -- https://www.hackerrank.com/challenges/name-of-employees/problem
 
-SELECT name
-FROM employee
-ORDER BY name;
+SELECT Name
+FROM Employee
+ORDER BY Name ASC
